dd76870e2d16eb7624aeb079489362b9c2e5efe7
musicbender <musicbender@gmail.com>
2016-03-30 15:54:31 -0700

    use ng-hide and ng-show to click start btn and show the first set of answers. Used a  variable and a . to access parent controller

--- js/main.js
+++ js/main.js
@@ -7,10 +7,14 @@ app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.quizNum = 0;
     $scope.audioNum = 0;
     $scope.score = 0;
+    $scope.quizHasBegun = false;
 }]);
 
 app.controller('startCtrl', ['$scope', function($scope) {
-
+    $scope.startQuiz = function () {
+        $scope.$parent.quizHasBegun = true;
+        console.log($scope.quizHasBegun);
+    }
 }]);
 
 app.controller('audioCtrl', ['$scope', function($scope) {

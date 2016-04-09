6d84910a430c0aedbae2a7fa771eb2f393bf91c9
musicbender <musicbender@gmail.com>
2016-04-09 07:35:51 -0700

    got rid of controller all together

--- js/main.js
+++ js/main.js
@@ -9,68 +9,67 @@
             serv.quizData = response.data;
         });
     }]);
-
-    app.controller('MainCtrl', ['$http', 'quiz', function($http, quiz) {
-        var vm = this;
-        
-        vm.quiz = quiz;
-        vm.quizNum = 0;
-        vm.audioNum = 0;
-        vm.score = 0;
-        vm.inProgress = false;
-        vm.quizOver = false;
-        vm.clicked = false;
-        vm.startQuiz = function() {
-            vm.quiz.inProgress = true;
-            console.log('TEST: start button clicked. ' + vm.quiz.inProgress);
-        }
-
-    }]);
-
-    app.directive('startSection', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/start.html',
-            replace: true
-        }
-    });
     
-    app.directive('scoreSection', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/score.html',
-            replace: true
-        }
-    });
-
-    app.directive('audioSection', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/audio.html',
-            replace: true
-        }
-    });
-
-    app.directive('answersSection', function() {
+    app.directive('answersSection', ['quiz', function(quiz) {
         return {
             restrict: 'E',
             templateUrl: 'templates/answers.html',
             replace: true,
             link: function(s, e, a) {
+                
+                s.quiz = quiz;
+                s.inProgress = false;
+                s.quizOver = false;
+                s.clicked = false;
+                s.startQuiz = function() {
+                    s.inProgress = true;
+                    s.quizNum = 0;
+                    s.audioNum = 0;
+                    s.score = 0;
+                    console.log('TEST: start button clicked. ' + s.inProgress);
+                    console.log('TEST: ' + s.quiz.quizData.data[0].box1.answer);
+                }
+                
                 s.checkAnswer = function(thisBox) {
                     console.log('checking answer...');
                     s.clicked = true;
                     s.nextSound();
                     if (thisBox.correct) {
-                        s.quiz.score++;
+                        s.score++;
                     } 
                 }
 
                 s.nextSound = function() {
-                    s.quiz.quizNum++;
+                    s.quizNum++;
                 }
             }
         }
-    });
+    }]);
+
+//    app.directive('startSection', function() {
+//        return {
+//            restrict: 'E',
+//            templateUrl: 'templates/start.html',
+//            replace: true
+//        }
+//    });
+//    
+//    app.directive('scoreSection', function() {
+//        return {
+//            restrict: 'E',
+//            templateUrl: 'templates/score.html',
+//            replace: true
+//        }
+//    });
+
+//    app.directive('audioSection', function() {
+//        return {
+//            restrict: 'E',
+//            templateUrl: 'templates/audio.html',
+//            replace: true
+//        }
+//    });
+
+    
 }());
 
